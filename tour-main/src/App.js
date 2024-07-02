31348503cfb83b4b64f74d5fc46f57d69a8b91d7
Coder-X <divyanshsingh1754@gmail.com>
2024-07-02 22:49:31 +0530

    Basic frontend with API call

--- tour-main/src/App.js
+++ tour-main/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
+import Navbar from "./components/common/Navbar"
+import Home from "./screens/Home"
+import Details from "./screens/Details"
+import Reservation from "./screens/Reservation"
+import Adventure from "./screens/Adventure"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/details/:id" element={<Details/>}/>
+        <Route path="/adventure/:id" element={<Adventure/>}/>
+        <Route path="/reservation" element={<Reservation/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
